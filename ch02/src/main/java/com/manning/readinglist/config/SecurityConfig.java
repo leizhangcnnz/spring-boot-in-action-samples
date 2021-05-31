39d63e98bbbea94610aefcc03748c88e32643e91
Lei Zhang <lei.zhang@kiwiplan.com>
2021-05-31 13:53:36 +1200

    initial customized security

---
+++ ch02/src/main/java/com/manning/readinglist/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.manning.readinglist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    }
+}
